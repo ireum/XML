9be8e1912083b0b6bec8917350939759c3abec71
ireum <wijaya.sudarta@competec.ch>
2017-08-15 14:42:58 +0200

    xml_3 loop

--- XML/xml_3.php
+++ XML/xml_3.php
@@ -1,29 +1,11 @@
 <?php
-$xmlDoc = new DOMDocument();
-$xmlDoc->load('product.xml');
-
 $counter = 1;
 
-$x = $xmlDoc->documentElement;
-foreach ($x->childNodes as $node) {
-//    $node->setAttribute('visited', 'true');
-//    $node->item(0);
+$xml = simplexml_load_file('product.xml');
+foreach ($xml->children() as $child) {
+    $child->addAttribute('visited', $counter++);
 }
+print $xml->saveXML();
 
-
-//$x = $xmlDoc->documentElement;
-//foreach ($x->childNodes as $item) {
-//    $item->nodeName->addAttribute('value', $counter++);
-//    print $item->nodeName . PHP_EOL;
-//}
-
-//$x = simplexml_load_file('product.xml');
-//$x->item->addAttribute('visited', 1);
-//print $x->saveXML();
-
-//$x = $xmlDoc->documentElement;
-//foreach ($x->childNodes as $item) {
-//        print $item->addAttribute('visited', $counter++);
-//}
 
 
